fa105266bee8a9dd512cad4e55c26814ce12202e
KurisuLim <kazuki1002003@yahoo.com>
2018-08-12 09:27:48 -0700

    Adds img code to display pictures

--- src/view/portfolio.js
+++ src/view/portfolio.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import '../css/App.css';
+import movieFinder from '../images/movieFinder.png';
+
 class Portfolio extends Component {
     render() {
       return (
@@ -8,6 +10,10 @@ class Portfolio extends Component {
           <div className='inner'>
           <div className='content'>
           <h2>Portfolio</h2>
+          <h4>Some of the latest work that I completed.</h4>
+          <a target='_blank' href='https://lim-react200-movie-finder.herokuapp.com/#/'>
+          <img scr={movieFinder}  className='movieFinder' alt='Movie Finder'/>
+          </a>
           </div>
           </div>
           </section>
